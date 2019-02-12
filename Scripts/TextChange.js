f0f25eaaa9d2b69116ba5ab218b357e6a226eb1c
Beppsi <46752177+Beppsi@users.noreply.github.com>
2019-02-12 09:00:25 +0100

    Clean up and fix

---
+++ Scripts/TextChange.js
@@ -0,0 +1,45 @@
+function Skrivut1(){
+  $.get("../Texter/societal.txt", function (data){
+    document.getElementById("content").innerHTML = data;
+  });
+};
+function Skrivut2(){
+  $.get("../Texter/#.txt", function (data){
+    document.getElementById("content").innerHTML = data;
+  });
+};
+function Skrivut3(){
+  $.get("../Texter/#.txt", function (data){
+    document.getElementById("content").innerHTML = data;
+  });
+};
+function Skrivut4(){
+  $.get("../Texter/#.txt", function (data){
+    document.getElementById("content").innerHTML = data;
+  });
+};
+function Skrivut5(){
+  $.get("../Texter/#.txt", function (data){
+    document.getElementById("content").innerHTML = data;
+  });
+};
+function Skrivut6(){
+  $.get("../Texter/#.txt", function (data){
+    document.getElementById("content").innerHTML = data;
+  });
+};
+function Skrivut7(){
+  $.get("../Texter/#.txt", function (data){
+    document.getElementById("content").innerHTML = data;
+  });
+};
+function Skrivut8(){
+  $.get("../Texter/#.txt", function (data){
+    document.getElementById("content").innerHTML = data;
+  });
+};
+function Skrivut9(){
+  $.get("../Texter/#.txt", function (data){
+    document.getElementById("content").innerHTML = data;
+  });
+};
