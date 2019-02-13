f128115dd36492a2ad693f05cc695d2cae1a99e1
Beppsi <46752177+Beppsi@users.noreply.github.com>
2019-02-13 16:06:51 +0100

    uwu

--- TextChange.js
+++ TextChange.js
@@ -1,55 +1,51 @@
-function Skrivut1(){
-  $.get("../Texter/.txt", function (data){
-    document.getElementById("content").innerHTML = data;
-  });
-};
+
 function Skrivut2(){
-  $.get("../Texter/Women.txt", function (data){
+  $.get("Texter/Women.txt", function (data){
     document.getElementById("content").innerHTML = data;
   });
 };
 function Skrivut3(){
-  $.get("../Texter/#.txt", function (data){
+  $.get("Texter/Economy.txt", function (data){
     document.getElementById("content").innerHTML = data;
   });
 };
 function Skrivut4(){
-  $.get("../Texter/Religion.txt", function (data){
+  $.get("Texter/Religion.txt", function (data){
     document.getElementById("content").innerHTML = data;
   });
 };
 function SkrivutS(){
-  $.get("../Texter/art.txt", function (data){
+  $.get("Texter/art.txt", function (data){
     document.getElementById("content").innerHTML = data;
   });
 };
 function Skrivut5(){
-  $.get("../Texter/#.txt", function (data){
+  $.get("Texter/#.txt", function (data){
     document.getElementById("content").innerHTML = data;
   });
 };
 function Skrivut6(){
-  $.get("../Texter/Culture.txt", function (data){
+  $.get("Texter/Culture.txt", function (data){
     document.getElementById("content").innerHTML = data;
   });
 };
 function Skrivut7(){
-  $.get("../Texter/societal.txt", function (data){
+  $.get("Texter/societal.txt", function (data){
     document.getElementById("content").innerHTML = data;
   });
 };
 function Skrivut8(){
-  $.get("../Texter/#.txt", function (data){
+  $.get("Texter/#.txt", function (data){
     document.getElementById("content").innerHTML = data;
   });
 };
 function Skrivut9(){
-  $.get("../Texter/politics.txt", function (data){
+  $.get("Texter/politics.txt", function (data){
     document.getElementById("content").innerHTML = data;
   });
 };
 function Skrivut10(){
-  $.get("../Texter/Tech.txt", function (data){
+  $.get("Texter/Tech.txt", function (data){
     document.getElementById("content").innerHTML = data;
   });
 };
